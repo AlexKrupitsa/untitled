72b39750ffc027ded8ca2cfd01fbf1268e0caf2a
krupitsaalex <krupica>
2020-10-30 12:32:17 +0300

    Домашнее задание 1

---
+++ src/Homework_2/Task_2_1.java
@@ -0,0 +1,25 @@
+package Homework_2;
+
+import java.util.Scanner;
+
+public class Task_2_1 {
+    public static void main(String[] args) {
+
+        int [] date ={31,29,31,30,31,30,31,31,30,31,30,31};
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter month");
+        int month = scanner.nextInt();
+
+        System.out.println("Enter date");
+        int day = scanner.nextInt();
+
+
+        if (day <= date [month-1]) {
+            System.out.println("This month has this date");
+        } else {
+            System.out.println("This month doesn't have this date");
+        }
+    }
+}
